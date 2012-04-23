15f5bf64465f62f9a7f87c689840ea373e458165
hagent <alextomin@list.ru>
2012-04-23 22:06:25 +0400

    create FixTheMoment project

---
+++ FixTheTime/src/tom/android/fixtime/FixTheTimeActivity.java
@@ -0,0 +1,14 @@
+package tom.android.fixtime;
+
+import tom.android.fixmoment.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FixTheTimeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
